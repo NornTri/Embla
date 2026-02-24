86cd93de24c3aa4cc6f0345889700384a12c4980
Mats Larsson <m.larsson88@gmail.com>
2026-02-25 00:48:37 +0100

    somesome

--- embla/users/tasks.py
+++ embla/users/tasks.py
@@ -1,9 +1,9 @@
 from celery import shared_task
 
-from .models import User
-
 
 @shared_task()
 def get_users_count():
     """A pointless Celery task to demonstrate usage."""
+    from .models import User  # noqa: PLC0415
+
     return User.objects.count()
